dbd625d1a51afd6afe24afa58d8f80ffb3cfb548
Brady Johnston <36021261+BradyAJohnston@users.noreply.github.com>
2025-09-15 12:07:27 +0100

    introduce deliberate bug

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Example repo for an example add-on!
 
-Subscript to this add-on here:
+Subscribe to this add-on here:
 
 ```
 https://bradyajohnston.github.io/monkeymadness/index.json
 ```
+
+Runt this build script before pushing to the repo to update the add-on.
 
 ## Build Script
 ```bash
--- monkeymadess/ops.py
+++ monkeymadess/ops.py
@@ -5,7 +5,7 @@ import random
 class MONKEYMADESS_OT_add_random_monkey(bpy.types.Operator):
     bl_idname = "monkeymadess.add_random_monkey"
     bl_label = "Add Random Monkey"
-    bl_description = "Add a Suzanne at a random location in (-10, 10)"
+    bl_description = "Add a Suzanne at a random location (-10,10); 1-in-5 add Torus"
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
@@ -14,7 +14,11 @@ class MONKEYMADESS_OT_add_random_monkey(bpy.types.Operator):
             random.uniform(-10.0, 10.0),
             random.uniform(-10.0, 10.0),
         )
-        bpy.ops.mesh.primitive_monkey_add(location=loc)
+        # 1 in 5 chance to add a torus instead
+        if random.randint(1, 5) == 1:
+            bpy.ops.mesh.primitive_torus_add(location=loc)
+        else:
+            bpy.ops.mesh.primitive_monkey_add(location=loc)
         return {"FINISHED"}
 
 
